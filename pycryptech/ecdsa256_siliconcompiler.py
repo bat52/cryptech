221d3b4808135e013290d5e6e040cdce998535ff
Marco Merlin <marcomerli@gmail.com>
2022-12-21 01:01:13 +0100

    added siliconcompiler cli

--- pycryptech/ecdsa256_siliconcompiler.py
+++ pycryptech/ecdsa256_siliconcompiler.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import sys
+import argparse
 import siliconcompiler as sc                      
 from ecdsa256_common import *
 from eda_common import get_source_files_alldir
@@ -20,22 +22,27 @@ def sc_get_pdks():
 def sc_setenv():
     os.environ['SCPATH'] = sc_path()
 
-    # export SCPATH=/home/marco/.local/lib/python3.10/site-packages/siliconcompiler/pdks
+def sc_cli(argv=[]):
+    parser = argparse.ArgumentParser(description='ECDSA256 SiliconCompiler flow Command Line Interface')
+    
+    # simulation options
+    parser.add_argument("-r",          "--remote_en"      , help="Run flow on remote server.", action='store_true' )
 
-def sc_main(remote_en = True):
+    p = parser.parse_args(argv)
+    return p    
+
+def sc_main(remote_en = False):
     #sc_setenv()
 
     chip = sc.Chip(TOPLEVEL)                      # create chip object
-    chip.set('input', 'verilog', get_source_files_alldir(INC_DIR_LIST))   # define list of source files
-    chip.add('input', 'verilog', get_source_files_alldir(SRC_DIR_LIST))   # define list of source files
+    chip.set('input', 'verilog', get_source_files_alldir(SRC_DIR_LIST + INC_DIR_LIST))  # define list of source files
     chip.set('input', 'sdc', CONSTRAINTS)         # set constraints file
     chip.load_target('freepdk45_demo')            # load predefined target
     chip.set('option', 'remote', remote_en)
     chip.run()                                    # run compilation
-    chip.summary()                                # print results summary
-    chip.show()                                   # show layout file
+    # chip.summary()                                # print results summary
+    # chip.show()                                   # show layout file
 
 if __name__ == '__main__':
-    # sc_setenv()
-    # print(os.environ['SCPATH'])
-    sc_main()
+    p = sc_cli(sys.argv[1:])
+    sc_main(p.remote_en)
