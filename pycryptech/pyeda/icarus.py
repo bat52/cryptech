148038ea2af01bfcef8d624080e03bd63ca4c606
Marco Merlin <marcomerli@gmail.com>
2022-12-30 10:20:49 +0100

    add custom_make method to custom_vpi

--- pycryptech/pyeda/icarus.py
+++ pycryptech/pyeda/icarus.py
@@ -26,16 +26,30 @@ def vpi_make(src_dirs=['./'], inc_dirs=[], args = []):
 class custom_vpi(object):
     work = './'
 
-    def __init__(self,tool='custom_vpi',url='',flist=[],inc_dirs=[],args=[]):
-        self.work = get_clean_work(tool=tool,rm_en=False) # do not remove if exist
-        self._get_custom_vpi(url=url,flist=flist)
+    def __init__(self,tool='custom_vpi',url='',flist=[],inc_dirs=[],args=[], custom_make_cmds=[]):
+        self._init_work(tool=tool)
+        self._get_custom_vpi(url=url,flist=flist)      
 
+        if len(custom_make_cmds)>0:
+            self.custom_make(cmds=custom_make_cmds)
+        else:
+            self.auto_make(inc_dirs=inc_dirs,args=args)
+
+    def _init_work(self,tool=''):
+        self.work = get_clean_work(tool=tool,rm_en=False) # do not remove if exist
+
+    def _get_custom_vpi(self,url,flist):
+        get_remote_files(url=url,flist=flist,dstdir=self.work)
+
+    def auto_make(self,inc_dirs=[],args=[]):
         new_inc = [os.path.join(self.work,ii) for ii in inc_dirs]
         vpi_make(src_dirs=[self.work], inc_dirs=new_inc,args=args)
         os.system('mv *.vpi %s' % self.work)
 
-    def _get_custom_vpi(self,url,flist):
-        get_remote_files(url=url,flist=flist,dstdir=self.work)
+    def custom_make(self,cmds=[]):
+        cmdstr = ' && '.join(cmds)
+        print(cmdstr)
+        os.system(cmdstr)
 
 class myhdl_vpi(custom_vpi):
     def __init__(self):
@@ -44,31 +58,17 @@ class myhdl_vpi(custom_vpi):
         flist = ['myhdl.c', 'myhdl_table.c', 'Makefile']
         custom_vpi.__init__(self,tool=tool,url=url,flist=flist)
 
-"""
 class fst_vpi(custom_vpi):
+   
     def __init__(self):
         tool='fst_vpi'
-        url="https://github.com/semify-eda/fstdumper/archive/refs/heads/main.zip"
-        inc_dirs=['./fstdumper-main/src', './fstdumper-main/src/config']
-        args=['-DICARUS_VERILOG','-fPIC','-O2']
-        custom_vpi.__init__(self,tool=tool,url=url,inc_dirs=inc_dirs,args=args)
-"""
-
-class fst_vpi(object):
-
-    work = './'
-
-    def __init__(self):
-        tool='fst_vpi'
-        url="https://github.com/semify-eda/fstdumper/archive/refs/heads/main.zip"
-
-        self.work = get_clean_work(tool=tool,rm_en=False) # do not remove if exist
-        get_remote_files(url=url,dstdir=self.work)
-
-        cmdstr = 'cd %s/fstdumper-main && make simulation-iverilog && mv *.vpi %s && cd ..' % (self.work, self.work)
-        print(cmdstr)
-
-        os.system(cmdstr)
+        url="https://github.com/semify-eda/fstdumper/archive/refs/heads/main.zip"        
+        self._init_work(tool=tool)
+        cmds = ['cd %s' % os.path.join(self.work,'fstdumper-main'),
+                'make simulation-iverilog',
+                'mv *.vpi %s' % self.work ,
+                'cd ..']
+        custom_vpi.__init__(self,tool=tool,url=url,custom_make_cmds=cmds)
 
 if __name__ == '__main__':
     # mv = myhdl_vpi()
